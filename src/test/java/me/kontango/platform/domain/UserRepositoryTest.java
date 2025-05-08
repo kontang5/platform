22e2413955692d2c6fa57fa0a7750a2cf883a9dd
jihoon <kontang5@icloud.com>
2025-05-08 21:06:57 +0900

    feat: configure JPA, add User entity, repository with test

---
+++ src/test/java/me/kontango/platform/domain/UserRepositoryTest.java
@@ -0,0 +1,31 @@
+package me.kontango.platform.domain;
+
+import me.kontango.platform.domain.user.User;
+import me.kontango.platform.domain.user.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+public class UserRepositoryTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    void shouldSaveAndLoadUser() {
+        User user = new User();
+        user.setName("John Doe");
+
+        userRepository.save(user);
+
+        List<User> users = new ArrayList<>(userRepository.findAll());
+        assertThat(users).isNotEmpty();
+        assertThat(users.get(0).getName()).isEqualTo("John Doe");
+    }
+}
