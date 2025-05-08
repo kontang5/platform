0d1ca380291cd82a65cb6659c339d7b95db1d1eb
jihoon <kontang5@icloud.com>
2025-05-08 20:31:21 +0900

    chore: add dependencies `Spring Data JPA` and `H2 Database`

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,8 +21,10 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("com.h2database:h2")
 
     developmentOnly("org.springframework.boot:spring-boot-devtools")
 
