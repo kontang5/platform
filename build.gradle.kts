c741902b55d35dc8b14a3add2b84e7cb932ac4d4
jihoon <kontang5@icloud.com>
2025-05-09 09:18:21 +0900

    chore: replace dependency `H2 Database` to `PostgreSQL Driver`

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,9 +22,9 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.postgresql:postgresql")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("com.h2database:h2")
 
     developmentOnly("org.springframework.boot:spring-boot-devtools")
 
